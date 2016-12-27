a0381ca9b49bee13fe7ca84b4c7380f07cdbe2c9
admin <admin@N12148D1>
2016-12-27 17:29:34 +0800

    7 test cases for Notifications

--- src/com/sio/webtestin/Notification.java
+++ src/com/sio/webtestin/Notification.java
@@ -3,20 +3,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class Notification {
-	@Test (priority=1)
-	public void DispalyRedPoint() {
+	public WebDriver driver;
+	String baseUrl = "http://10.120.1.39/";
+	@BeforeMethod
+	public void BeforeMethod(){
 		System.setProperty("webdriver.firefox.bin", "D:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-		
-		FirefoxProfile profile = new FirefoxProfile(); 
-		WebDriver driver = new FirefoxDriver(profile);
-		driver.get("http://10.120.1.39/");
-		
+		driver = new FirefoxDriver();
+		driver.get(baseUrl);
 		WebElement signupLogin = driver.findElement(By.className("login"));
 		signupLogin.click();
 		
@@ -39,7 +39,13 @@ public class Notification {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
+		
+		
+	}
+	////检查新消息红点标记（New）
+	@Test
+	public void DispalyRedPoint() {
 		
 		WebElement redpoint = driver.findElement(By.cssSelector(".notifications.new"));
 		if(redpoint.isDisplayed()){
@@ -52,8 +58,145 @@ public class Notification {
 		Assert.assertEquals("NEW", newmark.getText());
 		
 	}
-	//@Test
-	//public void ClickDropList(){
-		//WebElement droplist=
+	
+	/*private void moveEl(WebDriver driver,String xpath){
+		//检查评论消息
+		WebElement droplistvalue1 = driver.findElement(By.xpath(xpath));
+		Actions action = new Actions(driver);
+		action.moveToElement(droplistvalue1).perform();
+	}*/
+	
+	//检查下拉列表各个选项
+	@Test
+	public void ClickDropList(){
+		
+		//moveEl(driver, "/html/body/div[3]/div[2]/div/h1/div/div");
+		driver.findElement(By.className("notifications")).click();
+		//检查评论消息
+		WebElement droplistvalue = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/div"));
+		Actions action = new Actions(driver);
+		action.moveToElement(droplistvalue).perform();
+		try{
+			Thread.sleep(5000);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		
+		driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/ul/li[2]")).click();
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		WebElement comment1 = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[1]/div[1]/div[2]/span[1]"));
+		Assert.assertTrue(comment1.getText().contains("评论了你的直播"));
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//检查关注消息
+		action.moveToElement(droplistvalue).perform();
+		try{
+			Thread.sleep(4000);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		
+		driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/ul/li[3]")).click();
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement comment2 = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[2]/p[1]"));
+		Assert.assertTrue(comment2.getText().contains("关注了你"));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//检查赞消息
+		action.moveToElement(droplistvalue).perform();
+		try{
+			Thread.sleep(5000);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		
+		driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/ul/li[4]")).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(driver.getPageSource().contains("赞了你的直播"));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//检查转发消息
+		action.moveToElement(droplistvalue).perform();
+		try{
+			Thread.sleep(3000);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		
+	    driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/ul/li[5]")).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertTrue(driver.getPageSource().contains("转发了一个视频给你"));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//检查被转发消息
+		action.moveToElement(droplistvalue).perform();
+		try{
+			Thread.sleep(3000);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+			
+		}
+		
+	    driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/h1/div/ul/li[6]")).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Assert.assertTrue(driver.getPageSource().contains("转发了你的视频"));
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+}
 	
